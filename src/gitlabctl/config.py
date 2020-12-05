8efc7494acd9cbf06003627ddc931d38da13eafe
Thomas Bianchi <thomas.bianchi@mia-platform.eu>
2020-12-05 15:15:51 +0100

    config parser and tests

--- src/gitlabctl/config.py
+++ src/gitlabctl/config.py
@@ -8,25 +8,54 @@ __author__ = "Thomas Bianchi"
 __copyright__ = "Thomas Bianchi"
 __license__ = "mit"
 
-CONFIG_PATH = str(Path.home())
+CONFIG_PATH = Path.home()
 CONFIG_NAME = "gitlabctl.ini"
 
-config = configparser.ConfigParser()
 _logger = logging.getLogger(__name__)
 
 
-def save(filepath, config_dict):
-    config[config_dict["name"]] = {
-        "url": config_dict["url"],
-        "token": config_dict["token"]
-    }
-    with open(filepath, 'w') as configfile:
-        config.write(configfile)
+class Singleton(type):
+    _instances = {}
+
+    def __call__(cls, *args, **kwargs):
+        if cls not in cls._instances:
+            cls._instances[cls] = super(Singleton, cls). \
+                __call__(*args, **kwargs)
+        return cls._instances[cls]
 
 
-def read(filepath, section_name):
-    config.read(filepath)
-    config_dict = {"name": section_name, "url": config[section_name]["url"],
-                   "token": config[section_name]["token"]}
-    _logger.debug("config_dict: {}".format(config_dict))
-    return config_dict
+class Config(metaclass=Singleton):
+    def __init__(self, filepath=Path.joinpath(CONFIG_PATH, CONFIG_NAME)):
+        self.filepath = filepath
+        self.config = configparser.ConfigParser()
+        self._read(filepath)
+
+    def _read(self, filepath):
+        self.config.read(filepath)
+        return self.config
+
+    def save(self):
+        with open(self.filepath, 'w') as configfile:
+            self.config.write(configfile)
+
+    def set_current_context(self, context):
+        self.config['DEFAULT'] = {'current-context': context}
+
+    def _get_current_context(self):
+        return self.config.get('DEFAULT', 'current-context', fallback=None)
+
+    def get_config(self):
+        ctx = self._get_current_context()
+        if ctx:
+            return {
+                    'context': ctx,
+                    'url': self.config.get(ctx, 'url'),
+                    'token': self.config.get(ctx, 'token')
+                    }
+        return None
+
+    def set_context(self, context, url, token):
+        self.config[context] = {
+                                'url': url,
+                                'token': token
+                                }
--- tests/config_test.py
+++ tests/config_test.py
@@ -2,30 +2,100 @@
 
 from pathlib import Path
 
-import gitlabctl.config as config
+import pytest
+
+from gitlabctl.config import Config
 
 __author__ = "Thomas Bianchi"
 __copyright__ = "Thomas Bianchi"
 __license__ = "mit"
 
-CONTENT = """[gitTools]
+CONTENT = """[DEFAULT]
+current-context = gitTools
+
+[gitTools]
 url = https://ahahah.com
 token = kkkkkk
 
 """
 
+CONTENT2 = """[gitTools]
+url = https://ahahah.com
+token = kkkkkk
+"""
 
-def test_save_config(tmp_path):
-    config_file = Path.joinpath(tmp_path, "gitlabctl.ini")
-    config_dict = {"name": "gitTools", "url": "https://ahahah.com", "token": "kkkkkk"}
-    config.save(config_file, config_dict)
-    assert config_file.read_text() == CONTENT
+CONTENT3 = """[DEFAULT]
+current-context = gitTools
+[gitTools]
+url = https://ahahah.com
+token = kkkkkk
+[another]
+url = https://ssss.com
+token = sss
+"""
 
 
-def test_read_config(tmp_path):
+def write_config_example(tmp_path, content):
     config_file = Path.joinpath(tmp_path, "gitlabctl.ini")
     with open(config_file, "w") as f:
-        f.write(CONTENT)
-    config_dict = config.read(config_file, "gitTools")
-    assert config_dict["url"] == "https://ahahah.com"
-    assert config_dict["token"] == "kkkkkk"
+        f.write(content)
+    return config_file
+
+
+test_read_config_expections = [
+    pytest.param(CONTENT, {
+             'url': 'https://ahahah.com',
+             'context': 'gitTools',
+             'token': 'kkkkkk'
+             }, id='single-section'),
+    pytest.param(CONTENT3, {
+             'url': 'https://ahahah.com',
+             'context': 'gitTools',
+             'token': 'kkkkkk'
+             }, id='multiple-sections'),
+    pytest.param(CONTENT2, None, id='no-current'),
+]
+
+
+@pytest.mark.parametrize("a,expected", test_read_config_expections)
+def test_read_config(tmp_path, a, expected):
+    config_file = write_config_example(tmp_path, a)
+    cfg = Config(filepath=config_file)
+    # Config is a singleton, reinitialize for testing porpuses
+    cfg.__init__(filepath=config_file)
+    info = cfg.get_config()
+    print("info:", info)
+    assert info == expected
+
+
+def test_set_context(tmp_path):
+    cfg = Config(filepath=Path.joinpath(tmp_path, "gitlabctl.ini"))
+    cfg.__init__(filepath=Path.joinpath(tmp_path, "gitlabctl.ini"))
+    cfg.set_context('testSet', 'https://lll', 'eeeee')
+    assert 'testSet' in cfg.config.sections()
+    assert cfg.config['testSet']['url'] == 'https://lll'
+    assert cfg.config['testSet']['token'] == 'eeeee'
+
+
+def test_set_current_context(tmp_path):
+    cfg = Config(filepath=Path.joinpath(tmp_path, "gitlabctl.ini"))
+    cfg.__init__(filepath=Path.joinpath(tmp_path, "gitlabctl.ini"))
+    cfg.set_current_context('currcontextA')
+    assert cfg._get_current_context() == 'currcontextA'
+    assert cfg.config['DEFAULT']['current-context'] == 'currcontextA'
+
+
+def test_not_existing(tmp_path):
+    cfg = Config(filepath=Path.joinpath(tmp_path, "NonEsiste"))
+    cfg.__init__(filepath=Path.joinpath(tmp_path, "NonEsiste"))
+    assert not cfg.get_config()
+
+
+def test_save(tmp_path):
+    test_file = Path.joinpath(tmp_path, "gitlabctl.ini")
+    cfg = Config(filepath=test_file)
+    cfg.__init__(filepath=test_file)
+    cfg.config.read_string(CONTENT)
+    cfg.save()
+    with open(test_file) as f:
+        assert f.read() == CONTENT
